827670371ae9d1dfec78e8779fce5b78a5cc96be
yashwanth-creator02 <yashwanthsimha11@gmail.com>
2026-09-10 18:14:41 +0530

    chore: update dev script to bind to all interfaces and expand allowed development origins

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "10.100.23.151",
+    "10.100.23.151:3000",
+    "10.110.1.58",
+    "10.110.1.58:3000",
+    "localhost",
+    "localhost:3000",
+  ],
 };
-
-module.exports = {
-  allowedDevOrigins: ['10.110.1.58'],
-}
 
 export default nextConfig;
